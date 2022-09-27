169680e57bc56bcf51d8c0a7cd21e802ff856c82
crdSDE18 <crd110417@gmail.com>
2022-09-27 17:13:21 -0500

    completed best time to buy/sell git push git status

---
+++ leetcode/BestTimeToBuySell.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class BestTimeToBuySell {
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};//5
+        int[] prices2 = new int[]{1,2};//1
+        System.out.println(maxProfit(prices));
+    }
+
+    private static int maxProfit(int[] prices) {
+        int lsf = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        int currentProfitToday = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < lsf) {
+                lsf = prices[i];
+            }
+            currentProfitToday = prices[i] - lsf;
+            if (maxProfit < currentProfitToday) {
+                maxProfit = currentProfitToday;
+            }
+
+        }
+        return maxProfit;
+    }
+}
