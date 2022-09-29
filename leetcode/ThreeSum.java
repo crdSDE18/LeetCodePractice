685ed86fb8d933c2c7caff39cb26edce367a9758
crdSDE18 <crdoriety110417@gmail.com>
2022-09-29 17:25:58 -0500

    finally solved three sum

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -1,2 +1,41 @@
-package leetcode;public class ThreeSum {
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+
+    }
+
+    private static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> sum = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = nums.length - 1, target = 0;
+            while (left < right) {
+                int threeSum = nums[i] + nums[left] + nums[right];
+                if (threeSum > target) {
+                    right--;
+                } else if (threeSum < target) {
+                    left++;
+                } else {
+                    sum.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    while (nums[left] == nums[left - 1] && left < right) {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
 }
