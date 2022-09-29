90ed2590fb09a71f00c1dec7da82c927142d175c
crdSDE18 <crdoriety110417@gmail.com>
2022-09-28 19:47:29 -0500

    refactored containsDuplicateHashmap

--- leetcode/ContainsDuplicatesHashMap.java
+++ leetcode/ContainsDuplicatesHashMap.java
@@ -1,2 +1,26 @@
-package leetcode;public class ContainsDuplicatesHashMap {
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ContainsDuplicatesHashMap {
+
+    public static void main(String[] args){
+    int[] nums = new int[]{1,2,3,1};
+    System.out.println(hasDuplicates(nums));
+}
+
+    private static boolean hasDuplicates(int[] nums){
+        HashMap<Integer,Integer> dups = new HashMap<>();
+        for(int num:nums){
+            if(dups.containsKey(num)){
+                return true;
+
+            }
+            else{
+                dups.put(num,1);
+            }
+        }return false;
+    }
+
 }
