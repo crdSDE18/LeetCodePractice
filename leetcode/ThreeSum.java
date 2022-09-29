a3f04d2355708905ec52579554383335af53f73c
crdSDE18 <crdoriety110417@gmail.com>
2022-09-29 17:29:05 -0500

    added solution to 3sum

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -8,7 +8,7 @@ public class ThreeSum {
 
     public static void main(String[] args) {
         int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
-        System.out.println(threeSum(nums));
+        System.out.println(threeSum(nums));//[[-1, -1, 2], [-1, 0, 1]]
 
     }
 
