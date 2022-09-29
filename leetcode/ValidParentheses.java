fb141371d8fecce0d21623c85fec20e98926c64c
crdSDE18 <crdoriety110417@gmail.com>
2022-09-29 18:34:58 -0500

    is valid parentheses

--- leetcode/ValidParentheses.java
+++ leetcode/ValidParentheses.java
@@ -1,2 +1,44 @@
-package leetcode;public class ValidParentheses {
+package leetcode;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        String s1 = "()";
+        String s2 = "()[]{}";
+        String s3 = "(]";
+        String s4 = "(])";
+
+        System.out.println(isValid(s1));//true
+        System.out.println(isValid(s2)); //true
+        System.out.println(isValid(s3)); //false
+        System.out.println(isValid(s4));//false
+    }
+
+    private static  boolean isValid(String s){
+        char [] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(Character c: chars){
+            if(c.equals('{') || c.equals('[') || c.equals('(')){
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty()) {//considering check is empty at every if to save time
+                    return false;
+                }
+                if (c == '}' && stack.peek() != '{') {
+                    return false;
+                }
+                if (c == ']' && stack.peek() != '[') {
+                    return false;
+                }
+                if (c == ')' && stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
 }
