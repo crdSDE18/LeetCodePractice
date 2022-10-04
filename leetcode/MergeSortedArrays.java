3b966bacf07eeed371d446d20067c0b3c0eb0661
crdSDE18 <crdoriety110417@gmail.com>
2022-10-04 04:38:22 -0500

    added merge sorted array problem

---
+++ leetcode/MergeSortedArrays.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 3, 4, 5};
+        int arr2[] = {2, 6, 7, 8};
+
+        System.out.println(Arrays.toString(mergeSortedArrays(arr, arr2)));
+    }
+
+    private static int[] mergeSortedArrays(int[] num, int[] num1) {
+        int m = num.length;
+        int n = num1.length;
+        int result[] = new int[m + n];
+        int i = 0, j = 0, index = 0;
+
+        while (i < m && j < n) {//handles merging
+            if (num[i] < num1[j]) {
+                result[index] = num[i];
+                index++;
+                i++;
+            } else {
+                result[index] = num1[j];
+                index++;
+                j++;
+            }
+        }
+        //handles whatever is left
+        while (i < m) {
+            result[index] = num[i];
+            index++;
+            i++;
+        }
+
+        while (j < n) {
+            result[index] = num1[j];
+            index++;
+            j++;
+        }
+
+        return result;
+    }
+}
+
