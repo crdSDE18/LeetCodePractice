41efbb9ff1e28f067adbf13068f871c3e0362d61
crdSDE18 <crdoriety110417@gmail.com>
2022-10-13 03:53:50 -0500

    added can construct

---
+++ leetcode/CanConstruct.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CanConstruct {
+
+    public static void main(String[] args) {
+        String ransomNote = "a";
+        String magazine = "b";//false
+
+        System.out.println(canConstruct(ransomNote,magazine));
+        String ransomNoteA = "aa";
+        String ransomNoteB = "aab";//true
+
+        System.out.println(canConstruct(ransomNoteA,ransomNoteB));
+    }
+
+    private static boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char currentCharNote = ransomNote.charAt(i);
+                map.put(currentCharNote, map.getOrDefault(currentCharNote, 0) + 1);
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            char currentCharMag = magazine.charAt(i);
+            if (map.containsKey(currentCharMag)) {
+                map.put(currentCharMag, map.get(currentCharMag) - 1);
+                if (map.get(currentCharMag) == 0) {
+                    map.remove(currentCharMag);
+                }
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
